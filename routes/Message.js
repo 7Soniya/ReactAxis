4cccf1774d7ba30a6ff6bc58f23b9dafc2b98c9a
Pinku <py7953234@gmail.com>
2024-03-04 14:38:43 +0530

    first commit

--- routes/Message.js
+++ routes/Message.js
@@ -2,20 +2,37 @@ const express = require("express");
 const messageRecive = require("../models/Messages");
 const router = express.Router();
 
-// router.get("/message", async function (req, res) {
-//     const messagess = await messageRecive.find()
-//     res.json(messagess);
-// })
+router.get("/message", async function (req, res) {
+    const messagess = await messageRecive.find()
+    res.json(messagess);
+})
 
 router.get("/axismessage", async (req, res) => {
     let cardData = await messageRecive.find().sort({ createdAt: -1 });
     res.render("message", { cardData })
 })
 
-router.post("/message", async function (req, res) {
-    const messagedata = req.body;
-    await messageRecive.create(messagedata);
-    res.json("upadate")
+
+
+// router.post("/message", async function (req, res) {
+//     const messagedata = req.body;
+//     await messageRecive.create(messagedata);
+//     res.json("upadate")
+
+
+// })
+
+router.post('/message', async (req, res) => {
+    try {
+        var userDetails = new messageRecive({
+            message: req.body.message,
+        });
+        const message = await userDetails.save();
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+    res.send('message got success')
 })
 
 
